7a6e17f56d997291d922a71d3327440033110e61
Theo <brissettheo@gmail.com>
2024-09-25 11:38:04 +0200

    Finishing header look + starting homepage

--- templates/header.php
+++ templates/header.php
@@ -1,17 +1,22 @@
 <?php ob_start() ?>
 <header>
-    <div class="logo">
-        <p class="websiteName">PokéCheck</p>
-        <p>Check relationships between poké-types to compose your teams!</p>
+    <div>
+        <a href="/Pokemon-type-check/index.php?page=homepage">
+            <div class="logo">
+                <p class="websiteName">PokéCheck</p>
+                <p>Check relationships between poké-types to compose your teams!</p>
+            </div>
+        </a>
+        <nav aria-label="Main navigation">
+            <ul>
+                <li><a href="/Pokemon-type-check/index.php?page=homepage">Homepage</a></li>
+                <li><a href="/Pokemon-type-check/index.php?page=typevstype">Type vs type</a></li>
+                <li><a href="/Pokemon-type-check/index.php?page=pokemonvspokemon">Pokemon vs pokemon</a></li>
+                <li><a href="/Pokemon-type-check/index.php?page=equipevsequipe">Team vs team</a></li>
+            </ul>
+        </nav>
     </div>
-    <nav aria-label="Main navigation">
-        <ul>
-            <li><a href="/Pokemon-type-check/index.php?page=homepage">Homepage</a></li>
-            <li><a href="/Pokemon-type-check/index.php?page=typevstype">Type vs type</a></li>
-            <li><a href="/Pokemon-type-check/index.php?page=pokemonvspokemon">Pokemon vs pokemon</a></li>
-            <li><a href="/Pokemon-type-check/index.php?page=equipevsequipe">Team vs team</a></li>
-        </ul>
-    </nav>
+    <img class="pokeball" src="\Pokemon-type-check\img\icons\pokeball.png">
 </header>
 
 <?php $header = ob_get_clean() ?>
--- templates/homepage.php
+++ templates/homepage.php
@@ -8,8 +8,12 @@ $title = "Liste des pokémons";
 
 <?php ob_start() ?>
 <main>
-    <h2>Liste des pokémons disponibles</h2>
-    <p>Voici les pokémons actuellements enregistrés sur le site, n'hésitez pas à en proposer !</p>
+    <h1>Available pokemons list</h1>
+    <p>Here are the pokemon actually available to test on the website, feel free to add more if you have all the informations about them ! You can find them on the Bulbepedia website, the fanmade Pokemon Bible.</p>
+    <div>
+        <a class="button" href="https://bulbapedia.bulbagarden.net/wiki/Main_Page">Go to Pokemon Bible</a>
+        <a class="button button2" href="">Add a Pokemon</a>
+    </div>
     <div class="pokemonListe" >
         <?php 
         

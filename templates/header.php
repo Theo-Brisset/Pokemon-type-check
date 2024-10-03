16d8d1723190486ec2a81ffe579298f975fe2b10
Theo <brissettheo@gmail.com>
2024-10-03 16:14:00 +0200

    update lien nav

--- templates/header.php
+++ templates/header.php
@@ -9,14 +9,14 @@
         </a>
         <nav aria-label="Main navigation">
             <ul>
-                <li><a href="/Pokemon-type-check/index.php?page=homepage">Homepage</a></li>
-                <li><a href="/Pokemon-type-check/index.php?page=typevstype">Type vs type</a></li>
-                <li><a href="/Pokemon-type-check/index.php?page=pokemonvspokemon">Pokemon vs pokemon</a></li>
-                <li><a href="/Pokemon-type-check/index.php?page=equipevsequipe">Team vs team</a></li>
+                <li><a href="/pokemon-type-check/index.php?page=homepage">Homepage</a></li>
+                <li><a href="/pokemon-type-check/index.php?page=typevstype">Type vs type</a></li>
+                <li><a href="/pokemon-type-check/index.php?page=pokemonvspokemon">Pokemon vs pokemon</a></li>
+                <li><a href="/pokemon-type-check/index.php?page=equipevsequipe">Team vs team</a></li>
             </ul>
         </nav>
     </div>
-    <img class="pokeball" src="\Pokemon-type-check\img\icons\pokeball.png">
+    <img class="pokeball" src="\pokemon-type-check\img\icons\pokeball.png">
 </header>
 
 <?php $header = ob_get_clean() ?>
